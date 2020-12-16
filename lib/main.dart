23647f9b3dcdb5202ea313159561c841448052e2
hiruthic2002 <predatorsha2002@gmail.com>
2020-12-16 12:14:16 +0530

    Boilerplate front page

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  runApp(
+    MaterialApp(
+      title: 'eduserveMinimal',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.dark(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: MyHomePage(title: "eduserveMinimal"),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +24,233 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.black87,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.more_horiz_outlined),
+          onPressed: () {},
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            buildAttendenceContainer(_width, _height),
+            SizedBox(height: 10.0),
+            buildStudentInfoContainer(),
+            SizedBox(height: 10.0),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(
+                    left: _width / 30, right: _width / 30, top: _height / 60),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black12,
+                ),
+                child: ListView(
+                  children: [
+                    Center(child: Text("Leave Application's", style: TextStyle(fontSize: 20, color: Colors.purple[400]),)),
+                    SizedBox(height: 20.0),
+                    ListTile(
+                      title: Text("List item Active"),
+                      tileColor: Colors.green,
+                    ),
+                    SizedBox(height: 5.0),
+                    ListTile(
+                      title: Text("List item Pending"),
+                      tileColor: Colors.orange,
+                    ),
+                    SizedBox(height: 5.0),
+                    ListTile(
+                      title: Text("List item Rejected"),
+                      tileColor: Colors.red,
+                    ),
+                    SizedBox(height: 5.0),
+                    ListTile(
+                      title: Text("List item Rejected"),
+                      tileColor: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (index) {
+          print(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle),
+            label: "User",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildStudentInfoContainer() {
+    return Container(
+      // Basic student info
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Current Semester: ",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "4th",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Arrear's: ",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "5",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "ML Corrected: ",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "98.0",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "OD Corrected: ",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "97.0",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildAttendenceContainer(double width, double height) {
+    return Container(
+      // Attendance Details
+      color: Colors.black87,
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            // Class Attendence box
+            padding: EdgeInsets.only(left: width / 20, top: height / 30),
+            child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white24,
+                ),
+                width: width / 2.5,
+                height: height / 8,
+                child: Column(
+                  children: [
+                    Text(
+                      "Class Attendance",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.w800),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 35.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "98.0",
+                            style: TextStyle(
+                                fontSize: 30.0, color: Colors.green[300]),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.only(top: 10.0, bottom: 2.0),
+                              child: Text(
+                                "/100",
+                                style: TextStyle(fontSize: 15.0),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+          Padding(
+            // Assembly Attendence box
+            padding: EdgeInsets.only(left: width / 10, top: height / 30),
+            child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white24,
+                ),
+                width: width / 2.5,
+                height: height / 8,
+                child: Column(
+                  children: [
+                    Text(
+                      "Assembly Attendance",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.w800),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 35.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "45",
+                            style: TextStyle(fontSize: 15.0, color: Colors.red),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.only(top: 10.0, bottom: 2.0),
+                              child: Text(
+                                "/100",
+                                style: TextStyle(fontSize: 30.0),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
